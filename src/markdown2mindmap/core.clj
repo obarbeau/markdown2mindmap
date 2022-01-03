a12258256465344a288bf529d478a8fd1814002b
Olivier Barbeau - Rymint <olivier.barbeau@gmail.com>
2022-01-03 18:36:38 +0100

    refactor

--- src/markdown2mindmap/core.clj
+++ src/markdown2mindmap/core.clj
@@ -38,62 +38,64 @@
 ;; ------------------------------------
 
 (defn- enter
-  [where result]
-  (info (t/color-str :blue (str ">>enter-" where)) (dissoc @result :plantuml)))
+  [result ctype]
+  (info (t/color-str :blue (str ">>enter-" (name ctype))) (dissoc @result :plantuml)))
+
+(defn- now-inside
+  "We are inside a `ctype` element. Sets the `inside` flag to true
+   and the children."
+  [result ctype children]
+  (swap! result assoc-in [ctype :inside] true)
+  (if (number? children)
+    (swap! result assoc-in [ctype :children] children)
+    (swap! result update-in [ctype :children] children))
+  (enter result ctype))
 
 (defn- enter-heading
   "Enters a heading element. Level is given in attributes."
   [result [_elt-name attributes & children :as x]]
-  (let [level-heading (:level attributes)]
-    (swap! result assoc-in [:heading :level] level-heading)
-    (swap! result assoc-in [:heading :inside] true)
-    (swap! result assoc-in [:heading :children] (count children)))
-  (enter "heading" result)
+  (swap! result assoc-in [:heading :level] (:level attributes))
+  (now-inside result :heading (count children))
   x)
 
 (defn- enter-ol-ul
   "Enters a list, ordered or not."
   [result [_elt-name _attributes & children :as x]]
-  (swap! result assoc-in [:ul :inside] true)
-  (swap! result update-in [:ul :children] #(cons (count children) %))
-  (enter "ol-ul" result)
+  (now-inside result :ol-ul #(cons (count children) %))
   x)
 
 (defn- enter-li
   "Enters a list element. Cheat on the number of children; force to one."
+  ;; @todo why cheat????
   [result [:as x]]
-  (swap! result assoc-in [:li :inside] true)
-  (swap! result assoc-in [:li :children] 1)
-  (enter "li" result)
+  (now-inside result :li 1)
   x)
 
 (defn- enter-p
   "Enters a paragraph."
   [result [_elt-name _attributes & children :as x]]
-  (swap! result assoc-in [:p :inside] true)
-  (swap! result assoc-in [:p :children] (count children))
-  (enter "p" result)
+  (now-inside result :p (count children))
   x)
 
 (defn- enter-em
   "Enters an 'emphasize' modifier."
   [result [:as x]]
   (swap! result assoc :modifier :em)
-  (enter "em" result)
+  (enter result :em)
   x)
 
 (defn- enter-s
   "Enters a 'strike' modifier."
   [result [:as x]]
   (swap! result assoc :modifier :s)
-  (enter "s" result)
+  (enter result :s)
   x)
 
 (defn- enter-strong
   "Enters a 'strong' modifier."
   [result [:as x]]
   (swap! result assoc :modifier :strong)
-  (enter "strong" result)
+  (enter result :strong)
   x)
 
 (defn- apply-modifier
@@ -109,84 +111,107 @@
 
 ;; ------------------------------------
 
+(defn- last-simple-child?
+  "Returns a vector with 2 values:
+   1. True if it is the last child of a non-nestable element.
+   2. new number of children for this non-nestable element or zero."
+  [children]
+  (let [new-children (dec children)]
+    [(zero? new-children) new-children]))
+
+(defn- last-nested-child?
+  "Returns a vector with 2 values:
+   1. is it the last child of a nestable element (like ol/ul)?
+   2. rest of children or nil."
+  [children]
+  (let [[child1 & rest] children
+        child1dec (dec (or child1 1))]
+    [(zero? child1dec) (if (zero? child1dec)
+                         rest
+                         (cons child1dec rest))]))
+
+(defn- last-child?
+  "Returns a vector with 2 values:
+   1. is it the last child of the element of type `ctype`.
+   2. the new children number/list or nil."
+  [result ctype]
+  (let [children (get-in @result [ctype :children])
+        [last? new-children] (if (number? children)
+                               (last-simple-child? children)
+                               (last-nested-child? children))]
+    [last? new-children]))
+
+(defn- now-outside?
+  "Returns true if we are now completely outside of this (nested) element?"
+  [result ctype]
+  (let [[_last? new-children] (last-child? result ctype)]
+    (or (nil? new-children)
+        (and (number? new-children) (zero? new-children)))))
+
+(defn- inside-and-last?
+  "If we are we inside a `ctype` element, returns `last-child?` answer,
+  otherwise nil."
+  [result ctype]
+  (when (get-in @result [ctype :inside])
+    (last-child? result ctype)))
+
+;; ------------------------------------
+
 (defn- exit
-  [where result]
-  (info (t/color-str :purple (str "<<exit-" where)) (dissoc @result :plantuml)))
+  "Set `inside` flag to false
+   only if this was the last child of all (nested) elements."
+  [result ctype now-outside?]
+  (when now-outside?
+    (swap! result assoc-in [ctype :inside] false))
+  (info (t/color-str :purple (str "<<exit-" (name ctype))) (dissoc @result :plantuml)))
+
+(defn- exit-if-required
+  "If we are we inside a `ctype` element, sets the new number of children.
+  If it wad the last child, execute the `exit-fn`."
+  [result ctype exit-fn]
+  (let [[last? new-children] (inside-and-last? result ctype)]
+    (swap! result assoc-in [ctype :children] new-children)
+    (when last?
+      (exit-fn result))))
 
 (defn- exit-heading
   "Exit a heading element. Conj current buffer to plantuml text."
   [result]
-  (swap! result assoc-in [:heading :inside] false)
   (let [text (-> (get-in @result [:heading :level])
                  (repeat "*")
                  (concat " " (:buffer @result))
                  (#(apply str %)))]
     (swap! result update :plantuml conj text)
     (swap! result assoc :buffer ""))
-  (exit "heading" result))
-
-(defn- last-simple-child?
-  "True if it is the last child of a standard element."
-  [result ctype dec-children]
-  (swap! result assoc-in [ctype :children] dec-children)
-  (zero? dec-children))
-
-(defn- last-nested-child?
-  "True if it is the last child of a nestable element (like ol/ul).
-  Set `inside` flag to false
-  only if this was the last child of all nested elements."
-  [result ctype children]
-  (let [[child1 & rest] children
-        child1dec (dec (or child1 1))]
-    (if (zero? child1dec)
-      (do
-        (swap! result assoc-in [ctype :children] rest)
-        (when (nil? rest)
-          (swap! result assoc-in [ctype :inside] false))
-        true)
-      (do (swap! result assoc-in [ctype :children] (cons child1dec rest))
-          false))))
-
-(defn- last-child?
-  "true if it is the last child of the element of type `ctype`"
-  [result ctype]
-  (let [children (get-in @result [ctype :children])]
-    (if (number? children)
-      (last-simple-child? result ctype (dec children))
-      (last-nested-child? result ctype children))))
+  (exit result :heading true))
 
 (defn- exit-ol-ul
   "Exit a list, ordered or not."
   [result]
-  (exit "ol-ul" result))
+  (exit result :ol-ul (now-outside? result :ol-ul)))
 
 (defn- exit-li
   "Exit a list element.
   Use the level of the list added to the level of containing heading."
   [result]
-  (swap! result assoc-in [:li :inside] false)
   (let [level-heading (get-in @result [:heading :level])
-        level-ol-ul (count (get-in @result [:ul :children]))
+        level-ol-ul (count (get-in @result [:ol-ul :children]))
         level-total (+ level-heading level-ol-ul)
         text (-> (repeat level-total  "*")
                  (concat  "_ " (:buffer @result))
                  (#(apply str %)))]
     (swap! result update :plantuml conj text)
     (swap! result assoc :buffer "")
-    (exit "li" result)
-    (when (and (get-in @result [:ul :inside])
-               (last-child? result :ul))
-      (exit-ol-ul result))))
+    (exit result :li true)
+    (exit-if-required result :ol-ul exit-ol-ul)))
 
 (defn- exit-p
   "Exit a paragraph. The buffer is deleted if it has not been used."
   [result]
-  (swap! result assoc-in [:p :inside] false)
-  (exit "p" result)
+  (exit result :p true)
 
-  (when (and (get-in @result [:li :inside])
-             (last-child? result :li))
-    (exit-li result))
+  (exit-if-required result :li exit-li)
+
   (swap! result assoc :buffer ""))
 
 (defn- process-string
@@ -199,17 +224,13 @@
     (info "ignore string: " s)
     (do
       (swap! result update-in [:buffer] str (apply-modifier result s))
-      (info (t/color-str :blue ">>process-string") s @result)))
+      (info (t/color-str :blue ">>process-string") s (dissoc @result :plantuml))))
 
   (swap! result assoc :ignore-string false)
 
-  (when (and (get-in @result [:heading :inside])
-             (last-child? result :heading))
-    (exit-heading result))
+  (exit-if-required result :heading exit-heading)
 
-  (when (and (get-in @result [:p :inside])
-             (last-child? result :p))
-    (exit-p result))
+  (exit-if-required result :p exit-p)
   s)
 
 (defn- through-slb
@@ -217,11 +238,9 @@
   Then checks if this element was the last child of a paragraph
   and exit if required."
   [result x]
-  (when (get-in @result [:ul :inside])
+  (when (get-in @result [:ol-ul :inside])
     (swap! result assoc :ignore-string true))
-  (when (and (get-in @result [:p :inside])
-             (last-child? result :p))
-    (exit-p result))
+  (exit-if-required result :p exit-p)
   x)
 
 ;; ------------------------------------
@@ -276,7 +295,6 @@
 
     #_#_:else
       (infof "What is it? %s" x)))
-
 
 (defn md->hiccup
   "Converts markdown data to hiccup AST with cybermonday."
