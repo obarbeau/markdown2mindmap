a8fe654af416a774c0ce1baee391119a76810750
Olivier Barbeau - Rymint <olivier.barbeau@gmail.com>
2022-05-28 17:36:54 +0200

    add markdown2mindmap_convert helper

--- resources/functions-markdown2mindmap.zsh
+++ resources/functions-markdown2mindmap.zsh
@@ -2,10 +2,18 @@
 
 function markdown2mindmap_test() {
   cd $CLJ/markdown2mindmap
-  clojure -M:test/kaocha --watch
+  clojure -T:dev:build test-watch
 }
 
 function markdown2mindmap_repl() {
   cd $CLJ/markdown2mindmap
   clojure -M:dev:repl/rebel
+}
+
+function markdown2mindmap_convert() {
+  local input_file=$1
+  local output_dir=$2
+  cd $CLJ/markdown2mindmap
+  clojure -M:run-m convert --style resources/custom.css \
+    --type svg $input_file $output_dir
 }
